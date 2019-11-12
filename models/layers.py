4cef6a92ce6c91cc1b05205cf8b4249dcd544e5d
Polly <pdtarantsova@edu.hse.ru>
2019-11-12 18:10:26 +0300

    lstm layer

---
+++ models/layers.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+torch.manual_seed(1)
+
+
+class BiLSTM(nn.Module):
+
+    def __init__(self, vocab_size, embedding_dim, hidden_dim=64):
+        super(BiLSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.word_embeds = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim // 2, num_layers=1, bidirectional=True)
+
+    def forward(self, x):
+        embeds = self.word_embeds(x).view(len(x[0]), len(x), -1)
+        h_out, (_, _) = self.lstm(embeds)
+        return h_out
