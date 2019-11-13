150610260777283f3f92f8ab33f39f82e02c699e
Медведев Игорь Сергеевич <farshov@62.183.4.6.modem-pool.kuban.ru>
2019-11-13 13:50:46 +0300

    LSTM structure is done

---
+++ models/bilstm_model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class BiLSTMmodel(nn.Module):
+
+    def __init__(self):
+        super(BiLSTMmodel, self).__init__()
+
+    def forward(self):
+        pass
