e1096b050c61230ea5bb68ecdfc1eaa34c16edae
Медведев Игорь Сергеевич <farshov@62.183.4.6.modem-pool.kuban.ru>
2019-11-14 10:04:48 +0300

    scheduler for learning rate was fixed, simple lstm solution was implemented

--- models/bilstm_model.py
+++ models/bilstm_model.py
@@ -1,11 +1,28 @@
 import torch
 import torch.nn as nn
+from models.layers import BiLSTM
 
 
 class BiLSTMmodel(nn.Module):
 
-    def __init__(self):
+    def __init__(self, vocab_size, embedding_dim=100, n_slots=129, n_intents=29, hidden_dim=64):
         super(BiLSTMmodel, self).__init__()
 
-    def forward(self):
-        pass
+        self.voc_size = vocab_size
+        self.emb_dim = embedding_dim
+        self.hidden_size = hidden_dim
+        self.n_slots = n_slots
+        self.n_intetns = n_intents
+
+        self.rnn = BiLSTM(vocab_size, embedding_dim, hidden_dim=64)
+        self.slot_linear = nn.Linear(in_features=hidden_dim, out_features=n_slots)
+        self.intent_linear = nn.Linear(in_features=hidden_dim, out_features=n_intents)
+        self.relu = nn.ReLU()
+
+    def forward(self, x, lengths):
+        output = self.rnn(x, lengths)
+        output = self.relu(output)
+        slot = self.slot_linear(output)
+        intent = self.intent_linear(torch.sum(output, dim=1))
+        return torch.transpose(slot, 1, 2), intent
+        # (batch, n_slots, seq_len), (batch, n_intents)
--- models/dataset.py
+++ models/dataset.py
@@ -1,4 +1,6 @@
 from torch.utils import data
+import torch
+import numpy as np
 
 
 class ATIS(data.Dataset):
@@ -15,4 +17,39 @@ class ATIS(data.Dataset):
         return self.X[idx], self.slots[idx], self.intents[idx]
 
 
-def preprocess_atis()
+def preprocess_atis(raw_seq, raw_intent, raw_slot, n_intents, n_slots):
+    X = []
+    y_slot = []
+    y_intent = []
+    for i in range(len(raw_seq)):
+        X.append(torch.tensor([int(token) for token in raw_seq[0].iloc[i].split()]))
+
+        # intent = [0] * n_intents
+        # intent[raw_intent[i]] = 1
+        # y_intent.append(intent)  # (batch, INTENTS)
+        y_intent.append(raw_intent[0].iloc[i])  # (batch, 1)
+
+        slot = []  # двумерная матрица (SLOTS, seq_len)
+        # for j in range(len(raw_slot[0].iloc[i].split())):
+        #     cur_slot = [0] * n_slots
+        #     cur_slot[raw_slot[0].iloc[i].split()[j]] = 1
+        #     slot.append(torch.tensor(cur_slot))
+        slot = torch.tensor([int(token) for token in raw_slot[0].iloc[i].split()])
+
+        # как здесь стоит паддить - нулями, так как все равно использум кросс энтропию,
+        # а значит - эти нули не будут никак задействованы
+        y_slot.append(slot)  # (batch, seq_len, SLOTS)
+    y_intent = torch.tensor(y_intent)
+
+    return X, y_slot, y_intent
+
+
+def padding_map(X, padding_value):
+    """
+
+    :param X: tensor(batch, max_seq_len)
+    :param padding_value: token of pad
+    :return: list of len batch with values of appropriate lengths
+    """
+    return torch.sum((X != padding_value), dim=1)
+
